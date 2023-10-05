58492719fcfce29d82346f499527bf14f159af35
Villy Belmonte <vjbbelmont@gmail.com>
2023-10-05 15:02:48 -0700

    Modify handleChange function

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,13 +1,7 @@
 import { useState } from 'react'
 
-function handleChange(e, callback, type) {
-  if (type === 'change') {
-    callback(e.target.value);
-  } else if (type === 'select') {
-    callback(e.target.value);
-  } else if (type === 'pick') {
-    callback(e.target.value);
-  }
+function handleChange(e, callback) {
+  callback(e.target.value);
 }
 
 export function Input({ type, id, name, value, callback }) {
@@ -18,7 +12,7 @@ export function Input({ type, id, name, value, callback }) {
       id={id}
       name={name}
       value={value}
-      onChange={e => handleChange(e, callback, 'change')}
+      onChange={e => handleChange(e, callback)}
       />
   )
 }
@@ -33,7 +27,7 @@ export function InputIm({ type, id, name, value }) {
       id={id}
       name={name}
       value={text}
-      onChange={e => handleChange(e, setText, 'change')}
+      onChange={e => handleChange(e, setText)}
     />
   )
 }
@@ -42,7 +36,7 @@ export function InputSelectState({ id, name, select, callback }) {
   const states = [{"name":"Alabama","abbreviation":"AL"},{"name":"Alaska","abbreviation":"AK"},{"name":"Arizona","abbreviation":"AZ"},{"name":"Arkansas","abbreviation":"AR"},{"name":"California","abbreviation":"CA"},{"name":"Colorado","abbreviation":"CO"},{"name":"Connecticut","abbreviation":"CT"},{"name":"Delaware","abbreviation":"DE"},{"name":"Florida","abbreviation":"FL"},{"name":"Georgia","abbreviation":"GA"},{"name":"Hawaii","abbreviation":"HI"},{"name":"Idaho","abbreviation":"ID"},{"name":"Illinois","abbreviation":"IL"},{"name":"Indiana","abbreviation":"IN"},{"name":"Iowa","abbreviation":"IA"},{"name":"Kansas","abbreviation":"KS"},{"name":"Kentucky","abbreviation":"KY"},{"name":"Louisiana","abbreviation":"LA"},{"name":"Maine","abbreviation":"ME"},{"name":"Maryland","abbreviation":"MD"},{"name":"Massachusetts","abbreviation":"MA"},{"name":"Michigan","abbreviation":"MI"},{"name":"Minnesota","abbreviation":"MN"},{"name":"Mississippi","abbreviation":"MS"},{"name":"Missouri","abbreviation":"MO"},{"name":"Montana","abbreviation":"MT"},{"name":"Nebraska","abbreviation":"NE"},{"name":"Nevada","abbreviation":"NV"},{"name":"New Hampshire","abbreviation":"NH"},{"name":"New Jersey","abbreviation":"NJ"},{"name":"New Mexico","abbreviation":"NM"},{"name":"New York","abbreviation":"NY"},{"name":"North Carolina","abbreviation":"NC"},{"name":"North Dakota","abbreviation":"ND"},{"name":"Ohio","abbreviation":"OH"},{"name":"Oklahoma","abbreviation":"OK"},{"name":"Oregon","abbreviation":"OR"},{"name":"Pennsylvania","abbreviation":"PA"},{"name":"Rhode Island","abbreviation":"RI"},{"name":"South Carolina","abbreviation":"SC"},{"name":"South Dakota","abbreviation":"SD"},{"name":"Tennessee","abbreviation":"TN"},{"name":"Texas","abbreviation":"TX"},{"name":"Utah","abbreviation":"UT"},{"name":"Vermont","abbreviation":"VT"},{"name":"Virginia","abbreviation":"VA"},{"name":"Washington","abbreviation":"WA"},{"name":"West Virginia","abbreviation":"WV"},{"name":"Wisconsin","abbreviation":"WI"},{"name":"Wyoming","abbreviation":"WY"}];
   
   return (
-    <select name={name} id={id} value={select} onChange={e => {handleChange(e, callback, 'select')}}>
+    <select name={name} id={id} value={select} onChange={e => {handleChange(e, callback)}}>
       {select === 'default' && <option disabled value='default'>Select State</option>}
       {select !== 'default' && <option disabled>Select State</option>}
       {states.map( (state) => {
@@ -62,7 +56,7 @@ export function InputSelectState({ id, name, select, callback }) {
 
 export function InputSelectSkill({ id, name, select, callback }) {
   return (
-    <select name={name} id={id} defaultValue={select} onChange={e => {handleChange(e, callback, 'select')}}>
+    <select name={name} id={id} defaultValue={select} onChange={e => {handleChange(e, callback)}}>
       {select === 'default' && <option disabled value='default'>Select Skill Type</option>}
       {select !== 'default' && <option disabled>Select Skill Type</option>}
       <option key='soft' value='soft'>Soft</option>
@@ -92,7 +86,7 @@ function InputSelectFont({ id, name, select, callback }) {
   ]
 
   return (
-    <select name={name} id={id} value={select} onChange={e => {handleChange(e, callback, 'select')}}>
+    <select name={name} id={id} value={select} onChange={e => {handleChange(e, callback)}}>
       {fonts.map( (font) => {
         return (
           <option key={font.name} value={font.value}>{font.name}</option>
@@ -106,12 +100,12 @@ export function TextArea({ id, name, value, callback, type }) {
   const [text, setText] = useState(value);
   if (type === 'no-cb') {
     return (
-    <textarea id={id} name={name} rows='4' value={text} onChange={e => handleChange(e, setText, 'change')}>
+    <textarea id={id} name={name} rows='4' value={text} onChange={e => handleChange(e, setText)}>
     </textarea>
     )
   } else {
     return (
-      <textarea id={id} name={name} rows='4' value={value} onChange={e => handleChange(e, callback, 'change')}>
+      <textarea id={id} name={name} rows='4' value={value} onChange={e => handleChange(e, callback)}>
       </textarea>
     )
   }
@@ -120,7 +114,7 @@ export function TextArea({ id, name, value, callback, type }) {
 export function RadioImg({ id, name, value, src, state, callback }) {
   return (
     <label htmlFor={id}>
-      <input type='radio' id={id} name={name} value={value} checked={value === state} onChange={e => handleChange(e, callback, 'select')} />
+      <input type='radio' id={id} name={name} value={value} checked={value === state} onChange={e => handleChange(e, callback)} />
       <img className='radio-img' src={src} />
     </label>
   )
@@ -128,7 +122,7 @@ export function RadioImg({ id, name, value, src, state, callback }) {
 
 export function InputColor({ id, name, value, callback }) {
   return (
-    <input type='color' id={id} name={name} value={value} onChange={e => {handleChange(e, callback, 'pick')}} />
+    <input type='color' id={id} name={name} value={value} onChange={e => {handleChange(e, callback)}} />
   )
 }
   
